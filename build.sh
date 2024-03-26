fa5444bef92a045d2f32905f64aa244c9983957f
ra4ing <ra4ing@gmail.com>
2024-03-26 17:15:53 +0000

    base func of xsec

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # ./configure --prefix=/home/ra4ing/tools/riscv && echo "!!!!!!!!!!!!!!configured" && time make -j$(nproc) 2>&1 | tee build.log && echo "!!!!!!!!!!!!!!!!!!!!!!!!maked"
-./configure --prefix=/home/ra4ing/tools/riscv --with-arch=rv64imafdc_xo --with-abi=lp64d && echo "!!!!!!!!!!!!!!configured" && make newlib -j$(nproc) 2>&1 | tee build.log && echo "!!!!!!!!!!!!!!!!!!!!!!!!maked"
+./configure --prefix=/home/ra4ing/tools/riscv --with-arch=rv64imafdc_xs --with-abi=lp64d && echo "!!!!!!!!!!!!!!configured" && make newlib -j$(nproc) 2>&1 | tee build.log && echo "!!!!!!!!!!!!!!!!!!!!!!!!maked"
 # ./configure --prefix=/home/ra4ing/tools/riscv  --with-arch=rv64imafdc_xo --with-abi=lp64d --with-multilib-generator="rv64imafd-lp64d--c*xo" && echo "!!!!!!!!!!!!!!configured" && make newlib -j$(nproc) 2>&1 | tee build.log && echo "!!!!!!!!!!!!!!!!!!!!!!!!maked"
